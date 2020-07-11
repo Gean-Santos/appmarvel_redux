f8bde40b63542dc550396cdea1ce429d6e00e4bf
Gean-Santos <geansantos_15@hotmail.com>
2020-07-10 23:37:30 -0300

    modificando component Hero

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -4,24 +4,23 @@ const SCREEN_WIDTH = Dimensions.get('screen').width
 const SCREEN_HEIGHT = (Dimensions.get('screen').height)/2
 
 export default StyleSheet.create({
-  container: {
-    borderWidth: 1,
-    borderColor: 'red',
-  },
   image:{
     height: 60, 
     width: 60, 
-    borderRadius: 35,
+    borderRadius: 80,
   },
   text:{
     marginLeft: 20,
     color: '#FFF',
-    fontSize: 16
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   item:{
     flexDirection:'row', 
     padding: 10, 
     alignItems:'center',
-    backgroundColor: '#000',
+    backgroundColor: 'red',
+    borderRadius: 5,
+    margin: 10,
   },
 });
